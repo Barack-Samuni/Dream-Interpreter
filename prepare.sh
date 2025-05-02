e4ae5b83fab26ab378d3b4b8daa5dd240ba847d9
Michael Berger <vo1kod4v@gmail.com>
2025-05-02 12:00:56 +0000

    update requirements

--- prepare.sh
+++ prepare.sh
@@ -3,12 +3,4 @@ conda deactivate
 python -m venv .venv
 source .venv/bin/activate
 
-pip install transformers datasets
-pip install torch
-pip install pandas pandasql
-pip install matplotlib plotly
-pip install ipykernel tabulate
-pip install ipywidgets datasets
-
-pip install accelerate bitsandbytes tqdm
-pip install huggingface_hub
+pip install -r requirements.txt
